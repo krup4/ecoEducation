a595b3066a7cf41bf62ed970fe517b88823d6eb8
Krupnov <pavelkm10@gmail.com>
2024-09-18 22:32:55 +0300

    fix dockerfile

--- web/app/app.py
+++ web/app/app.py
@@ -2,7 +2,16 @@ from flask import Flask, jsonify
 
 from handlers.events import *
 
+from db_query.connect import connection
+
+from init_database import command
+
 app = Flask(__name__)
+
+with connection:
+    with connection.cursor(cursor_factory=extras.RealDictCursor) as cursor:
+        cursor.execute(command)
+        connection.commit()
 
 # app.register_blueprint(auth)
 app.register_blueprint(event, url_prefix='/event')
--- web/app/init_database.py
+++ web/app/init_database.py
@@ -1,6 +1,6 @@
 command = """
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS events
+DROP TABLE IF EXISTS events;
 
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
@@ -8,23 +8,23 @@ CREATE TABLE IF NOT EXISTS users (
     username TEXT,
     password TEXT,
     rating INT,
-    uuid uuid
+    uuid UUID
 );
 
 CREATE TABLE IF NOT EXISTS events (
     id SERIAL PRIMARY KEY,
-    uuid uuid,
+    uuid UUID,
     time TIMESTAMP,
     adress TEXT,
     name TEXT,
     cost INT,
     tags JSON,
-    description TEXT,
+    description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS qr_sessions (
     id SERIAL PRIMARY KEY,
-    session uuid,
+    session UUID,
     event_id INT,
     FOREIGN KEY (event_id) REFERENCES events(id)
 );
