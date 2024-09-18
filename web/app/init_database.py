1e06e3bc4b18b053ac3f91ce0d0f4da78d56e207
Krupnov <pavelkm10@gmail.com>
2024-09-18 18:56:49 +0300

    add events table

--- web/app/init_database.py
+++ web/app/init_database.py
@@ -1,5 +1,6 @@
 command = """
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS events
 
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
@@ -12,4 +13,16 @@ INSERT INTO users (login, password, isOrganizer) VALUES
     ('Afghanistan','iudvb', TRUE),
     ('oivnoerv','fduivodfv', FALSE),
     ('osidnv','iusdvs',TRUE);
+
+CREATE TABLE IF NOT EXISTS events (
+    id SERIAL PRIMARY KEY,
+    time TIMESTAMP,
+    latitude DECIMAL(9,6),
+    longitude DECIMAL(9,6),
+    name TEXT,
+    cost INT,
+    tags JSON,
+    description TEXT,
+    photo TEXT
+);
 """
