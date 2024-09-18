3dd9a5c1bd5695fcac6f25ac59a18bbfe45552ed
jtr1k <vip.banan.00@mail.ru>
2024-09-18 19:04:05 +0300

    amogussussybacka

---
+++ web/app/db_query/huyna.py
@@ -0,0 +1,23 @@
+
+import os
+import psycopg2
+from psycopg2 import extras
+import bcrypt
+
+salt = bcrypt.gensalt()
+
+user = os.environ['POSTGRES_USERNAME']
+password = os.environ['POSTGRES_PASSWORD']
+host = os.environ['POSTGRES_HOST']
+port = os.environ['POSTGRES_PORT']
+database = os.environ['POSTGRES_DATABASE']
+connection = psycopg2.connect(user=user,
+                              password=password,
+                              host=host,
+                              port=port,
+                              database=database)
+
+
+
+
+
