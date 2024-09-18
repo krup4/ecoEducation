a9bcf0831c660819ba4a4dcf4150eb9e3ec61229
jtr1k <vip.banan.00@mail.ru>
2024-09-18 18:27:16 +0300

    sigmacommit

--- web/app/app.py
+++ web/app/app.py
@@ -1,45 +1,7 @@
 from flask import Flask, jsonify
-import os
-import psycopg2
-from psycopg2 import extras
-import bcrypt
 
-from init_database import command
+from handlers import *
 
 app = Flask(__name__)
 
-salt = bcrypt.gensalt()
-
-user = os.environ['POSTGRES_USERNAME']
-password = os.environ['POSTGRES_PASSWORD']
-host = os.environ['POSTGRES_HOST']
-port = os.environ['POSTGRES_PORT']
-database = os.environ['POSTGRES_DATABASE']
-connection = psycopg2.connect(user=user,
-                              password=password,
-                              host=host,
-                              port=port,
-                              database=database)
-
-
-with connection:
-    with connection.cursor(cursor_factory=extras.RealDictCursor) as cursor:
-        cursor.execute(command)
-        connection.commit()
-
-
-@app.route('/ping', methods=['GET'])
-def send():
-    return jsonify({"status": "ok"}), 200
-
-
-
-
-@app.route('/users', methods=['GET'])
-def users():
-    query = "SELECT * FROM users"
-    with connection:
-        with connection.cursor(cursor_factory=extras.RealDictCursor) as cursor:
-            cursor.execute(query)
-            data = cursor.fetchall()
-            return jsonify(data), 200
+#app.register_blueprint(auth)
