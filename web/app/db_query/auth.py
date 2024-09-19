60af024365c2df96552d6934127bfd9ddeea27bd
Krupnov <pavelkm10@gmail.com>
2024-09-19 07:05:24 +0300

    fix auth

--- web/app/db_query/auth.py
+++ web/app/db_query/auth.py
@@ -19,34 +19,37 @@ connection = psycopg2.connect(user=user,
 
 
 def login(username):
-    cur = connection.cursor()    
+    cur = connection.cursor()
     cur.execute("SELECT * FROM users WHERE username=(%s)", (username,))
     data = cur.fetchone()
     cur.close()
     return data
 
+
 def auth_check(username, password):
     data = login(username)
-#    if (data is None):
-#        return False
-    return data
-    if ph.verify(password, data[2]):
-        return True
-    else:
+    if (data is None):
         return False
+    try:
+        ph.verify(data[3], password)
+        return True
+    except:
+        return False
+
 
 def register(username, password, full_name, rating):
     try:
         data = login(username)
-        #check if user exists
+        # check if user exists
         print(data)
         crazy = uuid.uuid4()
         cur = connection.cursor()
         h = ph.hash(password)
-        #return (username, str(h), full_name, rating, crazy)
-        cur.execute("INSERT INTO users (username, password, full_name, rating, uuid) VALUES (%s, %s, %s, %s, %s)", (username, str(h), full_name, rating, crazy))
+        # return (username, str(h), full_name, rating, crazy)
+        cur.execute("INSERT INTO users (username, password, full_name, rating, uuid) VALUES (%s, %s, %s, %s, %s)",
+                    (username, str(h), full_name, rating, crazy))
         cur.commit()
         cur.close()
-        
+
     except:
         return BrokenPipeError
--- web/app/handlers/auth.py
+++ web/app/handlers/auth.py
@@ -4,23 +4,25 @@ from handle_jwt import *
 
 auth = Blueprint("auth", __name__, url_prefix='/auth')
 
-@auth.route("/login", methods = ["POST"])
+
+@auth.route("/login", methods=["POST"])
 def login():
     username = request.json.get("username")
     password = request.json.get("password")
-    return jsonify({"asdf":auth_check(username, password)})
+    # return jsonify({"asdf":auth_check(username, password)})
     if auth_check(username, password):
-        return jsonify({"jwt": encode_jwt(username)}), 200
+        return jsonify({"status": "ok"}), 200
     else:
-        return jsonify({"error":"incorrect login/password"}), 400
+        return jsonify({"error": "incorrect login/password"}), 400
 
-@auth.route('/register', methods = ["POST"])
+
+@auth.route('/register', methods=["POST"])
 def register_user():
     username = request.json.get("username")
     password = request.json.get("password")
     full_name = request.json.get("full_name")
     try:
-        return jsonify({"asf":register(username, password, full_name, 0)})
-        return jsonify({"result":"OK"}), 200
+        return jsonify({"asf": register(username, password, full_name, 0)})
+        return jsonify({"result": "OK"}), 200
     except:
-        return jsonify({"result":"SOMETHING WORKS WRONG"}), 400
+        return jsonify({"result": "SOMETHING WORKS WRONG"}), 400
